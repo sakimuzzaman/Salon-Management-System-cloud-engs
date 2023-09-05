b29d28392d9a645b80e65a009304e250cf070c6d
Rafid-Al-Ahsan97 <rafidahsan78@gmail.com>
2023-09-05 15:42:00 +0600

    new updates

--- about.php
+++ about.php
@@ -1,143 +1,163 @@
-<!DOCTYPE HTML>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
-<script src="js/jquery.min.js"></script> 
-</head>
-<body>
-	<div class="header">
-		<div class="header_top">
-			<div class="wrap">
-			 <div class="logo">
-						<a href="index.php"><img src="images/47.png" alt="" /></a>
-					</div>
-			    <div class="call">
-			    	<p><img src="images/45.png" alt="" />Call US: 111-234-56789</p>
-			    </div>
-			  			 
-			<div class="clear"></div>
-  		</div>
-  	  </div>
-	<div class="header_bottom">
-		<div class="wrap">
-	     	<div class="menu">
-	     		<ul>
-			    	<li class="active"><a href="index.php">Home</a></li>
-			    	<li><a href="login.php">Login</a></li>
-			    	<li><a href="Customer_Add.php">Register</a></li>
-<li><a href="services.php">Services</a></li>			    	
-			    	<li><a href="about.php">About Us</a></li>
-			  
-			    	
-			    	<div class="clear"></div>
-     			</ul>
-	     	</div>
-			<div class="social-icons">
-	     		<ul>
-	     			<!----------------login------>
-	     		</ul>
-	     	</div>
-	     	<div class="clear"></div>
-	      </div>	     
-	  </div>	
-	   <div class="strip"> </div>
-    </div> 	     
-	  </div>
-	   <div class="main">
-    <div class="content">
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+?>
+<!doctype html>
+<html lang="en">
+  <head>
     
-    	 <div class="wrap">
-          
-    	 	<div class="image group">
-				
-				<div class="grid span_2_of_3">
-				
-<div class="contact_info">
-    	 				<h2>Find Us Here</h2>
-					    	  <div class="map">
-							  
-							   	    <iframe width="100%" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.in/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265&amp;output=embed"></iframe><br><small><a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265" style="color:#666;text-align:left;font-size:12px">View Larger Map</a></small>
-							  </div>
-      				</div>
-      			<div class="company_address">
-				     	<h2>Company Information :</h2>
-						    	<p>Reflection Salon & Spa,</p>
-						   		<p>Dhaka</p>
-						   		<p>Bangladesh</p>
-				   		<p>Phone:(00) 222 666 444</p>
-				   		<p>Fax: (000) 123 45 6 78</p>
-				 	 	<p>Email: <span>reflection@mycompany.com</span></p>
-				   		<p>Follow on: <span>Facebook</span>, <span>Twitter</span></p>
-				 </div>
-			  </div>			
-       </div>
-    </div>
- </div>
+    <title>Beauty Parlour Management System | About us Page</title>
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+  </head>
+  <body id="home">
+<?php include_once('includes/header.php');?>
+
+<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
+<!--bootstrap working-->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- //bootstrap working-->
+<!-- disable body scroll which navbar is in active -->
+<script>
+$(function () {
+  $('.navbar-toggler').click(function () {
+    $('body').toggleClass('noscroll');
+  })
+});
+</script>
+<!-- disable body scroll which navbar is in active -->
+
+<!-- breadcrumbs -->
+    <section class="w3l-inner-banner-main">
+        <div class="about-inner about ">
+            <div class="container">   
+                <div class="main-titles-head text-center">
+                <h3 class="header-name ">
+					About Us
+                </h3>
+            </div>
 </div>
-				</div>
-				</div>
-				</div>
-<div class="footer-strip"> </div>
- <div class="footer">
-   	  <div class="wrap">
-   	    <div class="footer_grides">
-   	    	<div class="footer_grid1">
-					<h3>Information</h3>
-								<ul>
-						            <li><a href="#">About Us</a></li>
-						     		<li><a href="#">Privacy Policy</a></li>
-						     		<li><a href="#">Newsletter</a></li>
-						     		<li><a href="#">Site Map</a></li>						     		
-						   	   </ul>	
-						
-					  	</div>
-				<div class="footer_grid2">
-					<h3>Get In Touch</h3>
-							<div class="address">
-							<ul>
-						  	 <li>Reflection Salon & Spa,</li>
-						  	  <li>New City,</li>
-						  	   <li>USA.</li>
-						  	 <li>www.reflection@gmail.com</li>
-						  	 <li><span>Telephone :</span> +06821458963</li>
-						  	 <li><span>Fax :</span> +01478523690</li>
-						  </ul>
-				   </div>				  
-			     </div>
-				<div class="footer_grid3">
-					<h3>Our Company</h3>
-						<div class="f_menu">
-							   <ul>
-						            <li><a href="#">About your Company</a></li>
-						     		<li><a href="#">Terms &amp; conditions</a></li>
-						     		<li><a href="#">News</a></li>
-						     		<li><a href="#">Team of professionals</a></li>	
-						     		<li><a href="#">Testimonials</a></li>					     		
-						   	   </ul>
-						</div>
-				   </div>				
-		  <div class="footer_grid4">
-			<h3>Follow US</h3>
-				<div class="img_list">
-				    <ul>
-					     <li><img src="images/28.png" alt=""><a href="#">Join Us on Facebook</a></li>
-					     <li><img src="images/twitter.png" alt=""><a href="#">Follow Us on Twitter</a></li>
-					     <li><img src="images/39.png" alt=""><a href="#">Share Us on Twitter</a></li>
-				    </ul>
-				</div>
-		 </div>
-	   <div class="clear"></div>
-     </div>
+   </div>
+   <div class="breadcrumbs-sub">
+   <div class="container">   
+    <ul class="breadcrumbs-custom-path">
+        <li class="right-side propClone"><a href="index.php" class="">Home <span class="fa fa-angle-right" aria-hidden="true"></span></a> <p></li>
+        <li class="active ">About</li>
+    </ul>
+</div>
+</div>
+        </div>
+    </section>
+<!-- breadcrumbs //-->
+<section class="w3l-content-with-photo-4"  id="about">
+    <div class="content-with-photo4-block ">
+        <div class="container">
+            <div class="cwp4-two row">
+            <div class="cwp4-image col-xl-6">
+                <img src="assets/images/b2.jpg" alt="product" class="img-responsive about-me">
+            </div>
+                <div class="cwp4-text col-xl-6 ">
+                    <div class="posivtion-grid">
+                    <h3 class="">Beauty and success starts here</h3>
+                    <div class="hair-two-colums">
+                        <div class="hair-left">
+<h5>
+    Waxing</h5>
+</div>
+                        <div class="hair-left">
+                            <h5>Facial</h5>
+                        </div>
+                            <div class="hair-left">
+                            <h5>Grooming</h5>
+
+                            </div>
+                                <div class="hair-left">
+                                    <h5>Massage</h5>
+                                    
+                                </div>
+                            <div class="hair-left">
+                                <h5>Menicure</h5>
+                            </div>
+
+   <div class="hair-left">
+                                                                                    <h5>Pedicure</h5>
+                                                                                </div>
+                                                                                   <div class="hair-left">
+                                                                                    <h5>Hair Cut</h5>
+                                                                                </div>
+
+                                                                                   <div class="hair-left">
+                                                                                    <h5>Body Spa</h5>
+                                                                                </div>
+
+                    
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
-  </div>	
-	     
-			<center><h1><b><font color="black"><i><pre></pre></b></center></h1></i></font><br><i><font size="3" color="brown">
-<div class="copy_right">
-				<p>© All rights Reseverd | Design by  <a href="http://w3layouts.com"> LIYA MATHEW </a></p>
-		 </div>
+</div>
+</section>
+
+<section class="w3l-recent-work">
+	<div class="jst-two-col">
+		<div class="container">
+<div class="row">
+		<div class="my-bio col-lg-6">
+
+	<div class="hair-make">
+		<?php
+
+$ret=mysqli_query($con,"select * from tblpage where PageType='aboutus' ");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+
+?>
+		<h5><a href="blog.html"><?php  echo $row['PageTitle'];?></a></h5>
+		<p class="para mt-2"><?php  echo $row['PageDescription'];?></p><?php } ?>
+	</div>
+	
+	
+	</div>
+	<div class="col-lg-6 ">
+		<img src="assets/images/b3.jpg" alt="product" class="img-responsive about-me">
+	</div>
+
+</div>
+		</div>
+	</div>
+</section>
+<?php include_once('includes/footer.php');?>
+<!-- move top -->
+<button onclick="topFunction()" id="movetop" title="Go to top">
+	<span class="fa fa-long-arrow-up"></span>
+</button>
+<script>
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("movetop").style.display = "block";
+		} else {
+			document.getElementById("movetop").style.display = "none";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+</script>
+<!-- /move top -->
 </body>
+
 </html>
-
-
-
--- login.php
+++ login.php
@@ -1,170 +1,176 @@
-﻿<!DOCTYPE HTML>
-<head>
-<title>Free Reflection Salon & span Website Template | Home :: w3layouts</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
-<script src="js/jquery.min.js"></script> 
-</head>
-<body>
-	<div class="header">
-		<div class="header_top">
-			<div class="wrap">
-			 <div class="logo">
-						<a href="index.php"><img src="images/47.png" alt="" /></a>
-					</div>
-			    <div class="call">
-			    	<p><img src="images/45.png" alt="" />Call US: 111-234-56789</p>
-			    </div>
-			  			 
-			<div class="clear"></div>
-  		</div>
-  	  </div>
-	<div class="header_bottom">
-		<div class="wrap">
-	     	<div class="menu">
-	     		<ul>
-			    	<li class="active"><a href="index.php">Home</a></li>
-			    	<li><a href="login.php">Login</a></li>
-			    	<li><a href="Customer_Add.php">Register</a></li>
-					<li><a href="services.php">Services</a></li>			    	
-			    	<li><a href="about.php">About Us</a></li>
-			    
-			    	<div class="clear"></div>
-     			</ul>
-	     	</div>
-	     	<div class="clear"></div>
-	      </div>	     
-	  </div>	
-	   <div class="strip"> </div>
-    </div> 	     
-	  </div>	
-    </div>  
-
- <div class="main">
-    <div class="content">
-    	 <div class="wrap">
-    	     <div class="contact-form">	
-					<h2>Login Form</h2><br>
-					    <form method="post" action="#">
-						    <div>
-						    	<span><label>USERNAME</label></span>
-						    	<span><input name="username" type="text" style="width: 44%;"></span>
-						    </div>
-						    <div>
-						     	<span><label>PASSWORD</label></span>
-						    	<span><input name="password" type="password" style="width: 44%;"></span>
-						    </div>
-                            <br>
-                            <br>
-                                <span><input type="submit" name="submit" value="submit" style="float:left;"></span><br><br><br>
-								  
-					    </form>  
-					  
-						
 <?php
-include "dbconnect.php";
-if(isset($_POST['submit']))
-{
- 	$uname=$_POST['username'];
- 	$pwd=$_POST['password'];
-	$p=md5($pwd);
-	$a=mysqli_query($con,"select * from `tbl_login` where `Username`='$uname' and `Password`='$p' or `Password`='$pwd'");
-	$row=mysqli_fetch_array($a);
-	if(!empty($row))
-	{
-		if($row['Role_id']==1)
-		{
-			session_start();
-			$_SESSION['username']=$row['Username'];
-			//$_SESSION['password']=$row['Password'];
-			$_SESSION['Reg_id']=$row['Reg_id'];
-			header("Location:customer_first.php?uid=$row[Reg_id]");
-		}
-		else if($row['Role_id']==2)
-		{
-			session_start();
-			$_SESSION['username']=$row['Username'];
-			//$_SESSION['password']=$row['Password'];
-			$_SESSION['Reg_id']=$row['Reg_id'];
-			header("Location:staff_first.php");
-			
-		}	
-		
-		else 
-		{	
-			session_start();
-			$_SESSION['username']=$row['Username'];
-			//$_SESSION['password']=$row['Password'];
-			$_SESSION['Reg_id']=$row['Reg_id'];
-			header("Location:admin_home.php");
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+error_reporting(0);
+
+if(isset($_POST['login']))
+  {
+    $emailcon=$_POST['emailcont'];
+    $password=md5($_POST['password']);
+    $query=mysqli_query($con,"select ID from tbluser where  (Email='$emailcon' || MobileNumber='$emailcon') && Password='$password' ");
+    $ret=mysqli_fetch_array($query);
+    if($ret>0){
+      $_SESSION['bpmsuid']=$ret['ID'];
+     header('location:index.php');
+    }
+    else{
+    echo "<script>alert('Invalid Details.');</script>";
+    }
+  }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+ 
+
+    <title>Beauty Parlour Management System | Login</title>
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+  </head>
+  <body id="home">
+<?php include_once('includes/header.php');?>
+
+<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
+<!--bootstrap working-->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- //bootstrap working-->
+<!-- disable body scroll which navbar is in active -->
+<script>
+$(function () {
+  $('.navbar-toggler').click(function () {
+    $('body').toggleClass('noscroll');
+  })
+});
+</script>
+<!-- disable body scroll which navbar is in active -->
+
+<!-- breadcrumbs -->
+<section class="w3l-inner-banner-main">
+    <div class="about-inner contact ">
+        <div class="container">   
+            <div class="main-titles-head text-center">
+            <h3 class="header-name ">
+                
+ Login Page
+            </h3>
+        </div>
+</div>
+</div>
+<div class="breadcrumbs-sub">
+<div class="container">   
+<ul class="breadcrumbs-custom-path">
+    <li class="right-side propClone"><a href="index.php" class="">Home <span class="fa fa-angle-right" aria-hidden="true"></span></a> <p></li>
+    <li class="active ">
+        Login</li>
+</ul>
+</div>
+</div>
+    </div>
+</section>
+<!-- breadcrumbs //-->
+<section class="w3l-contact-info-main" id="contact">
+    <div class="contact-sec	">
+        <div class="container">
+
+            <div class="d-grid contact-view">
+                <div class="cont-details">
+                    <?php
+
+$ret=mysqli_query($con,"select * from tblpage where PageType='contactus' ");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+
+?>
+                    <div class="cont-top">
+                        <div class="cont-left text-center">
+                            <span class="fa fa-phone text-primary"></span>
+                        </div>
+                        <div class="cont-right">
+                            <h6>Call Us</h6>
+                            <p class="para"><a href="tel:+44 99 555 42">+<?php  echo $row['MobileNumber'];?></a></p>
+                        </div>
+                    </div>
+                    <div class="cont-top margin-up">
+                        <div class="cont-left text-center">
+                            <span class="fa fa-envelope-o text-primary"></span>
+                        </div>
+                        <div class="cont-right">
+                            <h6>Email Us</h6>
+                            <p class="para"><a href="mailto:example@mail.com" class="mail"><?php  echo $row['Email'];?></a></p>
+                        </div>
+                    </div>
+                    <div class="cont-top margin-up">
+                        <div class="cont-left text-center">
+                            <span class="fa fa-map-marker text-primary"></span>
+                        </div>
+                        <div class="cont-right">
+                            <h6>Address</h6>
+                            <p class="para"> <?php  echo $row['PageDescription'];?></p>
+                        </div>
+                    </div>
+                    <div class="cont-top margin-up">
+                        <div class="cont-left text-center">
+                            <span class="fa fa-map-marker text-primary"></span>
+                        </div>
+                        <div class="cont-right">
+                            <h6>Time</h6>
+                            <p class="para"> <?php  echo $row['Timing'];?></p>
+                        </div>
+                    </div>
+               <?php } ?> </div>
+                <div class="map-content-9 mt-lg-0 mt-4">
+                    <form method="post">
+                        <div>
+                            <input type="text" class="form-control" name="emailcont" required="true" placeholder="Registered Email or Contact Number" required="true">
+                           
+                        </div>
+                        <div style="padding-top: 30px;">
+                          <input type="password" class="form-control" name="password" placeholder="Password" required="true">
+                        
+                        </div>
+                        
+                        <div class="twice-two" style="padding-top: 30px;">
+                          <a class="link--gray" style="color: blue;" href="forgot-password.php">Forgot Password?</a>
+                        
+                        </div>
+                        <button type="submit" class="btn btn-contact" name="login">Login</button>
+                    </form>
+                </div>
+    </div>
+   
+    </div></div>
+</section>
+<?php include_once('includes/footer.php');?>
+<!-- move top -->
+<button onclick="topFunction()" id="movetop" title="Go to top">
+	<span class="fa fa-long-arrow-up"></span>
+</button>
+<script>
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("movetop").style.display = "block";
+		} else {
+			document.getElementById("movetop").style.display = "none";
 		}
 	}
-	else
-		echo "<script>alert('Username/Password Incorrect')</script>"; 
-	}	
-?>
-				 
-		
-       
-     </div>
-    </div>
-  </div>
-  <div class="footer-strip"> </div>
- <div class="footer">
-   	  <div class="wrap">
-   	    <div class="footer_grides">
-   	    	<div class="footer_grid1">
-					<h3>Information</h3>
-								<ul>
-						            <li><a href="#">About Us</a></li>
-						     		<li><a href="#">Privacy Policy</a></li>
-						     		<li><a href="#">Newsletter</a></li>
-						     		<li><a href="#">Site Map</a></li>						     		
-						   	   </ul>	
-						
-					  	</div>
-				<div class="footer_grid2">
-					<h3>Get In Touch</h3>
-							<div class="address">
-							<ul>
-						  	  <li>Reflection Salon & Spa,</li>
-						  	  <li>New City,</li>
-						  	   <li>USA.</li>
-						  	 <li>www.reflection@gmail.com</li>
-						  	 <li><span>Telephone :</span> +06821458963</li>
-						  	 <li><span>Fax :</span> +01478523690</li>
-						  </ul>
-				   </div>				  
-			     </div>
-				<div class="footer_grid3">
-					<h3>Our Company</h3>
-						<div class="f_menu">
-							   <ul>
-						            <li><a href="#">About your Company</a></li>
-						     		<li><a href="#">Terms &amp; conditions</a></li>
-						     		<li><a href="#">News</a></li>
-						     		<li><a href="#">Team of professionals</a></li>	
-						     		<li><a href="#">Testimonials</a></li>					     		
-						   	   </ul>
-						</div>
-				   </div>				
-		  <div class="footer_grid4">
-			<h3>Follow US</h3>
-				<div class="img_list">
-				    <ul>
-					     <li><img src="images/28.png" alt=""><a href="#">Join Us on Facebook</a></li>
-					     <li><img src="images/twitter.png" alt=""><a href="#">Follow Us on Twitter</a></li>
-					     <li><img src="images/39.png" alt=""><a href="#">Share Us on Twitter</a></li>
-				    </ul>
-				</div>
-		 </div>
-	   <div class="clear"></div>
-     </div>
-    </div>
-  </div>
-<div class="copy_right">
-				<p>Company Name © All rights Reseverd | Design by  <a href="http://w3layouts.com"> LIYA MATHEW</a></p>
-		 </div>
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+</script>
+<!-- /move top -->
 </body>
+
 </html>
